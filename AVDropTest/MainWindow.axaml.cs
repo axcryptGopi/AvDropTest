10a51fed3f8557828621b0ec07573bf43f413006
Gopinath <Gopinath.b@axcrypt.net>
2026-02-26 12:47:52 +0530

    Drag drop code

--- AVDropTest/MainWindow.axaml.cs
+++ AVDropTest/MainWindow.axaml.cs
@@ -8,5 +8,18 @@ namespace AVDropTest
         {
             InitializeComponent();
         }
+
+        private void Window_DragEnter(object? sender, Avalonia.Input.DragEventArgs e)
+        {
+        }
+
+        private void Window_DragOver(object? sender, Avalonia.Input.DragEventArgs e)
+        {
+            e.DragEffects = Avalonia.Input.DragDropEffects.Move;
+        }
+
+        private void Window_Drop(object? sender, Avalonia.Input.DragEventArgs e)
+        {
+        }
     }
 }
